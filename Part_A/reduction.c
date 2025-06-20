75d7f9b5042f8370561905c37534f1bb81eca55b
teaching05 <teaching05@htc-login04.arcus-htc.arc.local>
2025-06-20 17:02:44 +0100

    Finished Friday

--- Part_A/reduction.c
+++ Part_A/reduction.c
@@ -1,22 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "omp.h"
 
 #define NUM_ELS 100000
 
 int main() {
-	
-    float random_array[NUM_ELS];
+    // initialise an array with dynamic memory allocation
+    // also initialise a variable to store sum in
+    float *random_array;
     float sum=0;
 
-    for(int i=0; i<NUM_ELS; i++) {
-        float x = ((float)rand())/((float)RAND_MAX);
-        random_array[i] = x;
-    }
+    random_array = (float*) malloc(NUM_ELS * sizeof(float));
 
-    for(int i=0; i<NUM_ELS; i++) {
-        sum+=random_array[i];
-    }
+    // initialise loop variable
+    int i;
 
+    // parallelise
+    #pragma omp parallel shared(sum, random_array) private(i)
+    {
+        #pragma omp for
+        for(i=0; i<NUM_ELS; i++) {
+            float x = ((float)rand())/((float)RAND_MAX);
+            random_array[i] = x;
+        }
+        #pragma omp for
+        for(int i=0; i<NUM_ELS; i++) {
+            sum+=random_array[i];
+        }
+    }
     printf("\nAverage:\t%f\n", sum/(float)NUM_ELS);
 
     return(0);
--- Part_A/reduction.cu
+++ Part_A/reduction.cu
@@ -3,21 +3,22 @@
 #include <string.h>
 #include <math.h>
 #include <float.h>
+#include <cuda.h>
+#include <curand.h>
 
-
-#define NUM_ELS 1024
+#define NUM_THREADS 1024 // max of 1024 threads per block on the A100
 
 __global__ void reduction(float *d_input, float *d_output)
 {
     // Allocate shared memory
 
-    __shared__  float smem_array[NUM_ELS];
-
+    __shared__  float smem_array[NUM_THREADS];
     int tid = threadIdx.x;
 
     // first, each thread loads data into shared memory
 
     smem_array[tid] = d_input[tid];
+    __syncthreads();
 
     // next, we perform binary tree reduction
 
@@ -27,8 +28,38 @@ __global__ void reduction(float *d_input, float *d_output)
     }
 
     // finally, first thread puts result into global memory
+    // this is because the final sum will be in element zero!
+    if (tid==0) {
+      d_output[blockIdx.x] = smem_array[0];
+    }
 
-    if (tid==0) d_output[0] = smem_array[0];
+}
+
+__global__ void reduction_final(float *d_input, float *d_output)
+{
+    // Allocate shared memory
+
+    __shared__  float smem_array[NUM_THREADS];
+    int tid = threadIdx.x;
+
+    // first, each thread loads data into shared memory
+
+    smem_array[tid] = d_input[tid];
+    __syncthreads();
+
+    // next, we perform binary tree reduction
+
+    for (int d = blockDim.x/2; d > 0; d /= 2) {
+      __syncthreads();  // ensure previous step completed 
+      if (tid<d)  smem_array[tid] += smem_array[tid+d];
+    }
+
+    // finally, first thread puts result into global memory
+    // this is because the final sum will be in element zero!
+    if (tid==0) {
+      d_output[0] = smem_array[0];
+    }
+
 }
 
 
@@ -39,47 +70,115 @@ __global__ void reduction(float *d_input, float *d_output)
 
 int main( int argc, const char** argv) 
 {
-    int num_els, num_threads, mem_size;
+    // Declare variables and arrays to store data
+    int num_els, num_blocks, mem_size, num_threads;
 
     float *h_data;
     float *d_input, *d_output;
 
+    // Ask user for how many numbers to implement reduction on
+    printf("Enter number of elements: ");
+    scanf("%d", &num_els);
+    printf("\n");
+
     // initialise card
+    int deviceid = 0;
+    int devCount;
+    cudaGetDeviceCount(&devCount);
+    if(deviceid<devCount){
+      cudaSetDevice(deviceid);
+    }
+    else {
+      printf("ERROR! Selected device is not available\n");
+      return(2);
+    }
 
-
-    num_els     = NUM_ELS;
-    num_threads = num_els;
+    // Detect whether number of elements is a power of two or not
+    int checker = num_els; // used to iterate and check whether num_els is a power of two or not
+    int exponent_of_two = 0; // how many powers of two constitute num_els
+    // e.g. if exponent_of_two = 4, then num_els has a factor of 2^4 = 16.
+    while (1) {
+      // if it indeed is a power of two...
+      if (checker / 2 == 1) {
+        // of 1024 or more, then let num_threads be 1024, i.e. one or more blocks used depending on num_els
+        if (num_els >= 1024) {
+	  num_threads = NUM_THREADS;
+        // of less than 1024, then let num_threads = num_els, i.e. one block only
+        } else {
+	  num_threads = num_els;
+	}
+	break;
+      // if it is a generic even number, then divide by two and record exponent_of_two
+      } else if (checker % 2 == 0) {
+        checker /= 2;
+        exponent_of_two++;
+        continue;
+      // if it is not a power of two...
+      } else {
+        // let num_threads = highest power-of-two factor
+        // this will mean num_blocks will be an odd number
+        num_threads = 1;
+	for (int i = 0; i < exponent_of_two; i++) {
+	  num_threads *= 2;
+	}
+        break;
+      }
+    }
+    
+    num_blocks = (int) num_els/num_threads;
     mem_size    = sizeof(float) * num_els;
 
     // allocate host memory to store the input data
-    // and initialize to integer values between 0 and 1000
-
     h_data = (float*) malloc(mem_size);
-      
+
+   /* RELIC FROM GIVEN CODE
     for(int i = 0; i < num_els; i++) {
         h_data[i] = ((float)rand()/(float)RAND_MAX);
     }
+    RELIC FROM GIVEN CODE */
 
     // allocate device memory input and output arrays
-
     cudaMalloc((void**)&d_input, mem_size);
-    cudaMalloc((void**)&d_output, sizeof(float));
+    cudaMalloc((void**)&d_output, sizeof(float)); // because the output is only one float at element zero!
 
+
+    // Generate randoms using CURAND
+    // Declare variable
+    curandGenerator_t gen;
+    // Create RNG
+    curandCreateGenerator(&gen, CURAND_RNG_PSEUDO_DEFAULT);
+    // Set the generator options
+    curandSetPseudoRandomGeneratorSeed(gen, 1234ULL);
+    // Generate the randoms
+    // This will create <num_els> amount of random numbers and put it into d_input
+    // Draw from a normal distribution with mean 5 and std 3
+    if (curandGenerateNormal(gen, d_input, num_els, 5.0f, 3.0f) != CURAND_STATUS_SUCCESS) {
+        printf("Something wrong :(");
+        return(1);
+    }
+
+
+    /* RELIC FROM GIVEN CODE
     // copy host memory to device input array
 
     cudaMemcpy(d_input, h_data, mem_size, cudaMemcpyHostToDevice);
+    RELIC FROM GIVEN CODE */
+
 
     // execute the kernel
-
-    reduction<<<1,num_threads>>>(d_input,d_output);
+    // reduce across threads in a block
+    reduction<<<num_blocks,NUM_THREADS>>>(d_input,d_output);
+    // reduce across the results from each block
+    // store the result back into d_input
+    reduction_final<<<1,num_blocks>>>(d_output, d_input);
 
     // copy result from device to host
 
-    cudaMemcpy(h_data, d_output, sizeof(float), cudaMemcpyDeviceToHost);
+    cudaMemcpy(h_data, d_input, sizeof(float), cudaMemcpyDeviceToHost);
 
     // check results
 
-    printf("reduction error = %f\n",h_data[0]/NUM_ELS);
+    printf("reduction error = %f\n",h_data[0]/num_els);
 
     // cleanup memory
 
@@ -91,4 +190,3 @@ int main( int argc, const char** argv)
 
     cudaDeviceReset();
 }
-
